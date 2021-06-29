edaeba01272241a5a10450b3d38ab2b0584da147
songnumber1 <songnumber1@naver.com>
2021-06-29 22:22:04 +0900

    Vue&Vuetify 강의 Ep05_2 - Form Validation 2탄 | vee-validate | 유효성검사

--- src/views/Forms.vue
+++ src/views/Forms.vue
@@ -6,9 +6,13 @@
 			</v-car-title>
 
 			<v-card-text>
-				<validation-observer>
-					<v-form>
-						<validation-provider name="Name" rules="max:10" v-slot="{ errors }">
+				<validation-observer ref="observer" v-slot="{ invalid }">
+					<v-form @submit.prevent="submit">
+						<validation-provider
+							name="Name"
+							rules="required|max:10"
+							v-slot="{ errors }"
+						>
 							<v-text-field
 								label="Name"
 								v-model="name"
@@ -18,23 +22,77 @@
 							</v-text-field>
 						</validation-provider>
 
-						<validation-provider>
-							<v-text-field label="PhoneNumber"> </v-text-field>
+						<validation-provider
+							name="PhoneNumber"
+							v-slot="{ errors }"
+							:rules="{
+								required: true,
+								numeric: true,
+								digits: 11
+							}"
+						>
+							<v-text-field
+								v-model="phoneNumber"
+								:counter="11"
+								label="PhoneNumber"
+								:error-messages="errors"
+							></v-text-field>
 						</validation-provider>
 
-						<validation-provider>
-							<v-text-field label="E-Mail"> </v-text-field>
+						<validation-provider
+							v-slot="{ errors }"
+							name="E-Mail"
+							:rules="{
+								required: true,
+								email: true
+							}"
+						>
+							<v-text-field
+								label="E-Mail"
+								v-model="email"
+								:error-messages="errors"
+							></v-text-field>
 						</validation-provider>
 
-						<validation-provider>
-							<v-select label="Select" :items="items"> </v-select>
+						<validation-provider
+							v-slot="{ errors }"
+							name="Select"
+							label="Select"
+							:rules="{
+								required: true
+							}"
+						>
+							<v-select
+								label="Select"
+								:items="items"
+								v-model="select"
+								:error-messages="errors"
+							>
+							</v-select>
 						</validation-provider>
 
-						<validation-provider>
-							<v-checkbox label="Checkbox" value="1"></v-checkbox>
+						<validation-provider
+							v-slot="{ errors }"
+							name="checkbox"
+							:rules="{
+								required: true
+							}"
+						>
+							<v-checkbox
+								label="Checkbox"
+								value="1"
+								v-model="checkbox"
+								:error-messages="errors"
+							></v-checkbox>
 						</validation-provider>
 
-						<v-btn type="submit" class="mr-4" color="primary">submit</v-btn>
+						<v-btn
+							type="submit"
+							class="mr-4"
+							color="primary"
+							:disabled="invalid"
+							>submit</v-btn
+						>
 						<v-btn>clear</v-btn>
 					</v-form>
 				</validation-observer>
@@ -45,7 +103,7 @@
 
 <script>
 import { extend, ValidationObserver, ValidationProvider } from 'vee-validate';
-import { max } from 'vee-validate/dist/rules';
+import { max, required, numeric, digits, email } from 'vee-validate/dist/rules';
 
 // extend('max', (value, params) => {
 // 	const limit = params[0];
@@ -60,6 +118,25 @@ extend('max', {
 	...max,
 	message: '{_field_}필드는 {length}자를 초과할 수 없습니다.'
 });
+
+extend('required', {
+	...required,
+	message: '{_field_}필드는 필수값입니다.'
+});
+extend('numeric', {
+	...numeric,
+	message: '{_field_}필드는 숫자로만 구성되어야합니다.'
+});
+extend('digits', {
+	...digits,
+	message: '{_field_}필드는 {length} 자리여야합니다.'
+});
+
+extend('email', {
+	...email,
+	message: '잘못된 이메일 주소입니다.'
+});
+
 export default {
 	name: 'Forms',
 	components: {
@@ -69,12 +146,34 @@ export default {
 	data() {
 		return {
 			name: null,
+			phoneNumber: null,
+			email: null,
+			select: null,
+			checkbox: null,
 			items: [
 				{ text: '아이템1', value: 1 },
 				{ text: '아이템2', value: 2 },
 				{ text: '아이템3', value: 3 }
 			]
 		};
+	},
+	methods: {
+		submit() {
+			this.$refs.observer.validate().then(result => {
+				console.log('result', result);
+
+				if (result) {
+					alert('양식 제출');
+				}
+			});
+		},
+		clear() {
+			this.name = null;
+			this.phoneNumber = null;
+			this.email = null;
+			this.select = null;
+			this.checkbox = null;
+		}
 	}
 };
 </script>
